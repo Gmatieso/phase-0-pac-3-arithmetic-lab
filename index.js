101f6667849ba4a7dbfb0596b6c2578c0eb9d3b2
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-08-10 14:41:39 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,60 @@
+function add(){
+
+}
+
+function subtract(){
+
+}
+
+function multiply(){
+
+}
+
+function divide(){
+
+}
+// adds a and b and return the results 
+function add(a,b){
+    return a + b 
+}
+
+// subtracts a and b and return the results 
+function subtract(a,b){
+    return a - b
+
+}
+
+// multiplies  a and b and return the results 
+function multiply(a,b){
+    return a * b
+
+}
+
+// divides  a and b and return the results 
+function divide(a,b) {
+    return  a / b 
+
+}
+
+// increment n and returns the results 
+function increment(n){
+    n +=1
+    return n  
+}
+// decrement n and returns the results 
+function decrement(n) {
+    n -=1
+    return n
+
+}
+
+
+function makeInt(n) {
+   return  parseInt(n, 10);
+
+}
+ 
+function preserveDecimal(n) {
+
+   return  parseFloat(n);
+} 
